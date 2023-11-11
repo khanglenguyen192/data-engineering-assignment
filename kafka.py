9017fefbe291bf9acf6888d0568940a0fe76443e
khanglenguyen192 <86032490+khanglenguyen192@users.noreply.github.com>
2023-11-11 21:19:54 +0700

    Update kafka.py

--- kafka.py
+++ kafka.py
@@ -9,7 +9,7 @@ topic = 'sample-topic'
 delay = 3
 
 def json_serializer(data):
-    return json.dumps(data)
+    return json.dumps(data).encode('utf-8')
 
 producer = KafkaProducer(
     bootstrap_servers = [kafka_server],
@@ -18,63 +18,63 @@ producer = KafkaProducer(
 
 #celcius
 def getTempData():
-    tempData = {
+    data = {
         "sensorId": random.randint(1, 5),
         "type": "TEMP",
         "value": random.randint(20, 35),
         "time": str(datetime.datetime.now())
     }
 
-    return json_serializer(tempData)
+    return data
 
 #percent
 def getHumidityData():
-    tempData = {
+    data = {
         "sensorId": random.randint(1, 5),
         "type": "HUMI",
         "value": round(random.uniform(20, 65), 2),
         "time": str(datetime.datetime.now())
     }
 
-    return json_serializer(tempData)
+    return data
 
 #pH
 def getSoilpHData():
-    tempData = {
+    data = {
         "sensorId": random.randint(1, 5),
         "type": "SOIL",
         "value": round(random.uniform(6.5, 7.5), 2),
         "time": str(datetime.datetime.now())
     }
 
-    return json_serializer(tempData)
+    return data
 
 #lit/minute
 def getWaterFlowData():
-    tempData = {
+    data = {
         "sensorId": random.randint(1, 5),
         "type": "WATER",
         "value": round(random.uniform(0, 2), 2),
         "time": str(datetime.datetime.now())
     }
 
-    return json_serializer(tempData)
+    return data
 
 while True:
     temp_data = getTempData()
-    print("Send to topic: "  + topic + " -----  data: " + temp_data)
-    producer.send(topic ,temp_data)
+    print("Send to topic: "  + topic + " -----  data: " + str(temp_data))
+    producer.send(topic, temp_data)
 
     humi_data = getHumidityData()
-    print("Send to topic: " + topic + " -----  data: " + humi_data)
+    print("Send to topic: " + topic + " -----  data: " + str(humi_data))
     producer.send(topic, humi_data)
 
     soil_ph_data = getSoilpHData()
-    print("Send to topic: " + topic + " -----  data: " + soil_ph_data)
+    print("Send to topic: " + topic + " -----  data: " + str(soil_ph_data))
     producer.send(topic, soil_ph_data)
 
     water_flow_data = getWaterFlowData()
-    print("Send to topic: " + topic + " -----  data: " + water_flow_data)
+    print("Send to topic: " + topic + " -----  data: " + str(water_flow_data))
     producer.send(topic, water_flow_data)
 
     time.sleep(delay)
